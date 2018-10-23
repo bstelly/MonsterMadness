32c5db263154f3fc0836e0b1f1246a6b9c343de1
Ralenski Doucet <Ralenskid@gmail.com>
2018-10-23 14:23:32 -0500

    started on Attack classes

---
+++ Assets/Scripts/AttackBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehaviourScript : MonoBehaviour {
+
+    public int damageAmount;
+
+    public int DamageAmount
+    {
+        get { damageAmount; }
+        set { damageAmount = value; }
+    }
+
+    public void doDamage(int value)
+    {
+        int health = 100;
+        for(health>0;value>0;health-value)
+        {
+            health > 0 - value <= 100;
+            return health;
+        }
+    }
+}
---
+++ Assets/Scripts/MeleeAttackTypeBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackTypeBehaviourScript : MonoBehaviour {
+    int attackPower;
+    int AttackPower
+    {
+        get
+        {
+            AttackPower = attackPower;
+        }
+        set
+        {
+            attackPower = value;
+        }
+    }
+}
