a0a0e9f593f6043683028f9e6cc967fe8af0bff7
matthewguillory65 <matthewguillory65@gmail.com>
2018-10-23 14:40:24 -0500

    Starting ig

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = getComponent<RigidBody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+}
