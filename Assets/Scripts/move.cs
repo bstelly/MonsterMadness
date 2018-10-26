411ce33b6bdde4b58321fa44c1e8c112be922447
bstelly <bstelly1999@gmail.com>
2018-10-26 16:32:11 -0500

    animating kong

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -6,12 +6,15 @@ public class move : MonoBehaviour
 {
 
     public HealthScriptable health;
-
+    bool walking;
+    public GameEvent onPunch;
+    public Animation punch;
 	// Use this for initialization
 	void Start ()
 	{
 	    health.health = 100;
 	    health.maxHealth = 100;
+        walking = false;
 	}
 	
 	// Update is called once per frame
@@ -19,16 +22,33 @@ public class move : MonoBehaviour
 	    if (Input.GetKey(KeyCode.A))
 	    {
 	        transform.position += new Vector3(-0.1f, 0);
+            walking = true;
 	    }
+        else
+        {
+            walking = false;
+        }
 	    if (Input.GetKey(KeyCode.D))
 	    {
 	        transform.position += new Vector3(0.1f, 0);
-	    }
+            walking = true;
 
-	    if (Input.GetKey(KeyCode.Space))
+        }
+        else
+        {
+            walking = false;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
 	    {
 	        health.health -= 2;
+            onPunch.Raise();
 	    }
 
+    }
+
+    public void PlayPunchAnim()
+    {
+        punch.Play();
     }
 }
